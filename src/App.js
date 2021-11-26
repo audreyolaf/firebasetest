4863c48ce8e3f5086860ff380b3a3ab3f2366171
Audrey Wang <audreywang2006@gmail.com>
2021-11-25 19:24:51 -0800

    iteration 3

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import "./App.css";
 import { onSnapshot, collection, addDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import db from "./firebase";
-
-const Dot = ({ color }) => {
-  const style = {
-    height: 25,
-    width: 25,
-    margin: "0px 10px",
-    backgroundColor: color,
-    borderRadius: "50%",
-    display: "inline-block",
-  };
-  return <span style={style}></span>;
-};
+import Dot from "./Dot";
+import { handleEdit, handleNew } from "./util";
 
 function App() {
   const [colors, setColors] = useState([{ name: "Loading", id: "initial" }]);
@@ -28,16 +18,6 @@ function App() {
     []
   );
 
-  const handleNew = async () => {
-    const name = prompt("Enter color name");
-    const value = prompt("Enter color value");
-
-    const collectionRef = collection(db, "colors", "");
-    const payload = { name, value };
-    const docRef = addDoc(collectionRef, payload);
-    console.log("The new ID:" + docRef.id);
-  };
-
   return (
     <div className="root">
       <button className="button" onClick={handleNew}>
@@ -47,7 +27,10 @@ function App() {
       <ul>
         {colors.map((color) => (
           <li key={color.id}>
-            <a href="#">edit</a> <Dot color={color.value} /> {color.name}
+            <a href="#" onClick={() => handleEdit(color.id)}>
+              edit
+            </a>{" "}
+            <Dot color={color.value} /> {color.name}
           </li>
         ))}
       </ul>
