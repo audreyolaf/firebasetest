befcec10b22467bc9db4db9e60da4f91106fa1da
Audrey Wang <audreywang2006@gmail.com>
2021-11-25 20:03:35 -0800

    iteration 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { onSnapshot, collection, addDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import db from "./firebase";
 import Dot from "./Dot";
-import { handleEdit, handleNew } from "./util";
+import { handleNew, handleEdit, handleDelete, handleQueryDelete } from "./util";
 
 function App() {
   const [colors, setColors] = useState([{ name: "Loading", id: "initial" }]);
@@ -23,13 +23,19 @@ function App() {
       <button className="button" onClick={handleNew}>
         New
       </button>
+      <button className="button" onClick={handleQueryDelete}>
+        Query Delete
+      </button>
 
       <ul>
         {colors.map((color) => (
           <li key={color.id}>
-            <a href="#" onClick={() => handleEdit(color.id)}>
+            <button className="button2" onClick={() => handleEdit(color.id)}>
               edit
-            </a>{" "}
+            </button>
+            <button className="button2" onClick={() => handleDelete(color.id)}>
+              delete
+            </button>
             <Dot color={color.value} /> {color.name}
           </li>
         ))}
--- src/util.js
+++ src/util.js
@@ -1,4 +1,13 @@
-import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+  deleteDoc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import db from "./firebase";
 
 export const handleNew = async () => {
@@ -18,4 +27,23 @@ export const handleEdit = async (id) => {
   const docRef = doc(db, "colors", "", id);
   const payload = { name, value };
   setDoc(docRef, payload);
+};
+
+export const handleDelete = async (id) => {
+  const docRef = doc(db, "colors", id);
+  await deleteDoc(docRef);
+};
+
+export const handleQueryDelete = async (id) => {
+  const userInputName = prompt("Enter color name:");
+
+  const collectionRef = collection(db, "colors");
+  const q = query(collectionRef, where("name", "==", userInputName));
+
+  const snapshot = await getDocs(q);
+  const results = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  results.forEach(async (result) => {
+    const docRef = doc(db, "colors", result.id);
+    await deleteDoc(docRef);
+  });
 };
